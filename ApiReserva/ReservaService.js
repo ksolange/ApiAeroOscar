9603081aee2a02b91a66acc23f62c828474edb26
Karen Alfaro <ksalfarom@gmail.com>
2022-11-06 14:58:32 +0100

    postman y reservas al día

--- ApiReserva/ReservaService.js
+++ ApiReserva/ReservaService.js
@@ -2,13 +2,13 @@
 let reservas = require("./reservas.json")
 let request = require("axios")  // 05/11/2022 request es solicitud q se hace
 
-const reservaGet = () => {
+const reservasGet = () => {
     
     return reservas
 
 }
 
-const reservaSet = async (reserva) => {  // envío al request una url  + reserva + id vuelo
+const reservasSet = async (reserva) => {  // envío al request una url  + reserva + id vuelo
     console.log("Llama a reserva a guardar")
     await request.get( // es una promesa
         "http://localhost:8081/vuelos/id/?id=" + reserva.idvuelo
@@ -22,21 +22,21 @@ const reservaSet = async (reserva) => {  // envío al request una url  + reserva
         }
     )
     console.log(reserva)
-    reserva.push(reserva)      // debería estar lo nuevo al final de la cola por el push
+    reservas.push(reserva)      // debería estar lo nuevo al final de la cola por el push
     console.log(reservas)
     return reservas
 }
 
-const reservaDelete = (id) => {
-    console.log(reserva)
-    reserva = reserva.filter((vuel) => { // es en una línea el return se hace ( ), si es más d una línea hacer el  {} true false
+const reservasDelete = (id) => {
+    console.log(reservas)
+    reservas = reservas.filter((vuel) => { // es en una línea el return se hace ( ), si es más d una línea hacer el  {} true false
         return vuel.id != id
         }
     )
-    console.log(reserva)
-    return reserva
+    console.log(reservas)
+    return reservas
 }
 
-module.exports.reservagetExport = reservaGet;         //exportar
-module.exports.reservaSetExport = reservaSet; 
-module.exports.reservaDeleteExport = reservaDelete; 
+module.exports.reservasgetExport = reservasGet;         //exportar
+module.exports.reservasSetExport = reservasSet; 
+module.exports.reservasDeleteExport = reservasDelete; 
